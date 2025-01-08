0673380aa947bc39f49d3305ac0a762056fa3216
Isla Koenigsknecht <ikoenigbu@gmail.com>
2025-01-08 08:30:39 -0500

    Add back debug logging

--- packages/mobile/ios/NodeJsMobile/NodeRunner.mm
+++ packages/mobile/ios/NodeJsMobile/NodeRunner.mm
@@ -205,7 +205,7 @@ id appPauseEventsManagerSetLock = [[NSObject alloc] init];
     nodePath = [nodePath stringByAppendingString:builtinModulesPath];
   }
   setenv([@"NODE_PATH" UTF8String], (const char*)[nodePath UTF8String], 1);
-  setenv([@"DEBUG" UTF8String], "backend*,quiet*,state-manager*,desktop*,utils*,identity*,common*,libp2p:connection-manager:auto-dial", 1);
+  setenv([@"DEBUG" UTF8String], "backend*,quiet*,state-manager*,desktop*,utils*,identity*,common*,libp2p*,helia*,blockstore*,datastore*", 1);
 
   int c_arguments_size=0;
 
