c5a9c04cb0cb6aec09369a0f188e980e8ac2ac98
Isla Koenigsknecht <ikoenigbu@gmail.com>
2025-01-28 16:55:02 -0500

    Add encryption and decryption of messages

--- packages/backend/src/nest/storage/channels/messages/orbitdb/MessagesAccessController.ts
+++ packages/backend/src/nest/storage/channels/messages/orbitdb/MessagesAccessController.ts
@@ -15,7 +15,10 @@ import { getCrypto } from 'pkijs'
 import { stringToArrayBuffer } from 'pvutils'
 import { keyObjectFromString, verifySignature } from '@quiet/identity'
 import { ChannelMessage, NoCryptoEngineError } from '@quiet/types'
-import { posixJoin } from './util'
+import { posixJoin } from '../../../orbitDb/util'
+import { EncryptedMessage } from '../messages.types'
+import { SigChainService } from 'packages/backend/src/nest/auth/sigchain.service'
+import { MessagesService } from '../messages.service'
 
 const codec = dagCbor
 const hasher = sha256
@@ -43,7 +46,7 @@ const AccessControlList = async ({
 const type = 'messagesaccess'
 
 export const MessagesAccessController =
-  ({ write }: { write: string[] }) =>
+  ({ write, messagesService }: { write: string[]; messagesService: MessagesService }) =>
   async ({ orbitdb, identities, address }: { orbitdb: OrbitDBType; identities: IdentitiesType; address: string }) => {
     const storage = await ComposedStorage(
       await LRUStorage({ size: 1000 }),
@@ -65,7 +68,7 @@ export const MessagesAccessController =
     const crypto = getCrypto()
     const keyMapping: Map<string, CryptoKey> = new Map()
 
-    const canAppend = async (entry: LogEntry<ChannelMessage>) => {
+    const canAppend = async (entry: LogEntry<EncryptedMessage>) => {
       if (!crypto) throw new NoCryptoEngineError()
 
       const writerIdentity = await identities.getIdentity(entry.identity)
@@ -85,15 +88,7 @@ export const MessagesAccessController =
       const message = entry.payload.value
 
       if (message) {
-        const signature = stringToArrayBuffer(message.signature)
-        let cryptoKey = keyMapping.get(message.pubKey)
-
-        if (!cryptoKey) {
-          cryptoKey = await keyObjectFromString(message.pubKey, crypto)
-          keyMapping.set(message.pubKey, cryptoKey)
-        }
-
-        return await verifySignature(signature, message.message, cryptoKey)
+        return messagesService.verifyMessage(message)
       } else {
         return true
       }
