d18014d66d08915cd8872f5e756376a10eb8987f
taea <taelxvie@gmail.com>
2025-02-05 17:03:46 -0500

    handle permissions errors during invite creation and display appropriate screens when non-admins try to admit users

--- packages/desktop/src/renderer/components/Settings/Tabs/QRCode/QRCode.tsx
+++ packages/desktop/src/renderer/components/Settings/Tabs/QRCode/QRCode.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
 import { Site } from '@quiet/common'
 import { connection } from '@quiet/state-manager'
 
 import { QRCodeComponent } from './QRCode.component'
+import { createLogger } from 'packages/desktop/src/renderer/logger'
+
+const logger = createLogger('QRCode')
 
 export const QRCode: React.FC = () => {
   const dispatch = useDispatch()
@@ -18,7 +24,7 @@ export const QRCode: React.FC = () => {
 
   useEffect(() => {
     if (invitationReady) {
-      setInvitationLink(inviteLink || Site.MAIN_PAGE)
+      setInvitationLink(inviteLink)
     }
   }, [invitationReady, inviteLink])
 
